15827e524dc220edfa31ec4006ef455199450d02
zweizhao <zweizhao@foxmail.com>
2024-12-04 12:50:30 +0000

    feat 可运行腾讯云函数

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ if (global.isDev) {
   AV.init({
     appId: process.env.LEANCLOUD_APP_ID || '',
     appKey: process.env.LEANCLOUD_APP_KEY || '',
+    // @ts-ignore
+    serverURL: process.env.LEANCLOUD_SERVER_URL || '',
     masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || ''
   })
 }
@@ -61,9 +63,6 @@ app.use('/', routes)
 
 module.exports = app // vercel的@vercel/node需要, 且不需要listen
 
-if (global.isDev) {
-  // 调试用
-  app.listen(PORT, () => {
-    console.log(`服务器运行在 http://localhost:${PORT}`)
-  })
-}
+app.listen(PORT, () => {
+  console.log(`服务器运行在 http://localhost:${PORT}`)
+})
